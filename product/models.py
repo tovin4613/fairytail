27331b3dfb3e577b706870635c970c5315364ccf
inha <qaqrs99@gmail.com>
2023-12-20 13:22:18 +0900

    comment,media

--- product/models.py
+++ product/models.py
@@ -12,10 +12,24 @@ class BookList(models.Model):
         return self.name
     
 class Post(models.Model):       #<-추가
-    User_id = models.CharField(max_length=100)      #<-추가
-    title = models.CharField(max_length=20)     #<-추가
-    content = models.CharField(max_length=20)       #<-추가
+    User_id = models.CharField()      #<-추가
+    title = models.CharField()     #<-추가
+    content = models.CharField()       #<-추가
     created_at = models.DateTimeField(auto_now_add=True)        #<-추가
 
     def __str__(self):      #<-추가
         return self.name        #<-추가
+
+class comment(models.Model):
+    Post_id = models.IntegerField()
+    User_id = models.CharField()
+    content = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    
+class Media(models.Model):
+    Post_id = models.IntegerField()
+    media_path = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
--- product/serializer.py
+++ product/serializer.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from product.models import Post
+from product.models import Post, comment, Media
 
 class boardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post    
         fields = ['id', 'User_id', 'title', 'content', 'created_at']
+class commentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = comment
+        fields = ['id', 'Post_id', 'User_id', 'content', 'created_at']
+class MediaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Media
+        fields = ['id', 'Post_id', 'media_path', 'created_at']
