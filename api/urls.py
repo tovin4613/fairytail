53ec3ed10a87b002f633a8ec377e147a01c44b02
ldj <tovin4613@gmail.com>
2023-12-26 11:02:57 +0900

    BookList API 수정

--- api/urls.py
+++ api/urls.py
@@ -13,8 +13,9 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     #path('', include(router.urls)),
     path('BookList/', BookListView.as_view()),
-    path('BookList2/', BookListView2.as_view()),
-    path('BookList2/<int:pk>', BookListDetailView2.as_view()),
+    path('BookList/<int:pk>/', BookListDetailView.as_view()),
+    # path('BookList2/', BookListView2.as_view()),
+    # path('BookList2/<int:pk>', BookListDetailView2.as_view()),
     # path('BookList/<int:pk>/', BookListRetrieveView.as_view()),
     path('BookDetail/', BookDetailView.as_view()),
     path('QuizList/', QuizListView.as_view()),
