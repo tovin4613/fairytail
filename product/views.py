e3a35626c68a7b79b566e63043a5c5a60fb952f3
inha <qaqrs99@gmail.com>
2023-12-26 14:05:31 +0900

    comment작성수정

--- product/views.py
+++ product/views.py
@@ -59,3 +59,42 @@ class postRetrieveview(APIView):
         book = self.get_object(pk)
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)      
+    
+class commentuploadview(APIView):
+    authentication_classes = [BasicAuthentication, SessionAuthentication, JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    def get_object(self, pk):
+        try:
+            return comment.objects.get(pk=pk)
+        except comment.DoesNotExist:
+            raise Http404
+    
+    # 조회
+    def get(self, request, pk,id, format=None):
+        book = self.get_object(id)
+        serializer = commentSerializer(book)
+        return Response(serializer.data)
+
+    # 등록
+    def post(self, request, pk, id, format=None):
+        book = self.get_object(pk)
+        serializer = commentSerializer(data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 수정
+    def put(self, request, pk, id, format=None):
+        book = self.get_object(id)
+        serializer = commentSerializer(book, data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 삭제
+    def delete(self, request, id, format=None):
+        book = self.get_object(id)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
